3c7bbc3f36a2cfc81f63e207b8c17aaa7691b07b
Vesa <vesainwonderland@gmail.com>
2021-05-25 16:00:52 +0300

    osa1 1.9

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -12,12 +12,7 @@ const App = (props) => {
       <Button text="good" handleClick={() => setGoodCounter(goodCount + 1)} />
       <Button text="neutral" handleClick={() => setNeutralCounter(neutralCount + 1)} />
       <Button text="bad" handleClick={() => setBadCounter(badCount + 1)} />
-      <div>
-        <h1>statistics</h1>
-      </div>
-      <Display value={"good"} counter={goodCount} />
-      <Display value={"neutral"} counter={neutralCount} />
-      <Display value={"bad"} counter={badCount} />
+      
       <Statistics goodCount={goodCount} neutralCount={neutralCount} badCount={badCount} />
     </div>
 
@@ -25,8 +20,23 @@ const App = (props) => {
 }
 
 const Statistics = ({ goodCount, neutralCount, badCount }) => {
+  if (goodCount === 0 && neutralCount === 0 && badCount === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+
   return (
     <div>
+      <div>
+        <h1>statistics</h1>
+      </div>
+      <Display value={"good"} counter={goodCount} />
+      <Display value={"neutral"} counter={neutralCount} />
+      <Display value={"bad"} counter={badCount} />
       <Display value={"all"} counter={goodCount + neutralCount + badCount} />
       <Display value={"average"} counter={(goodCount - badCount) / (goodCount + neutralCount + badCount)} />
       <Display value={"positive"} counter={goodCount / (goodCount + neutralCount + badCount) + " %"} />
