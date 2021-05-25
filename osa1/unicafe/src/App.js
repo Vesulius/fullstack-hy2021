5a40c9200cd8427390f0bb5644187072f3172f87
Vesa <vesainwonderland@gmail.com>
2021-05-25 15:34:48 +0300

    osa1 1.8

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,38 +1,44 @@
 import React, { useState } from 'react'
 const App = (props) => {
-  const [ goodCount, setGoodCounter ] = useState(0)
-  const [ neutralCount, setNeutralCounter ] = useState(0)
-  const [ badCount, setBadCounter ] = useState(0)
-  // const [ average, setAverage ] = useState(0)
+  const [goodCount, setGoodCounter] = useState(0)
+  const [neutralCount, setNeutralCounter] = useState(0)
+  const [badCount, setBadCounter] = useState(0)
 
   return (
     <div>
       <div>
         <h1>give feedback</h1>
       </div>
-      <Button text="good" handleClick={() => setGoodCounter(goodCount + 1)}/>
-      <Button text="neutral" handleClick={() => setNeutralCounter(neutralCount + 1)}/>
-      <Button text="bad" handleClick={() => setBadCounter(badCount + 1)}/>
+      <Button text="good" handleClick={() => setGoodCounter(goodCount + 1)} />
+      <Button text="neutral" handleClick={() => setNeutralCounter(neutralCount + 1)} />
+      <Button text="bad" handleClick={() => setBadCounter(badCount + 1)} />
       <div>
         <h1>statistics</h1>
       </div>
-      <Display value={"good"} counter={goodCount}/>
-      <Display value={"neutral"} counter={neutralCount}/>
-      <Display value={"bad"} counter={badCount}/>
+      <Display value={"good"} counter={goodCount} />
+      <Display value={"neutral"} counter={neutralCount} />
+      <Display value={"bad"} counter={badCount} />
+      <Statistics goodCount={goodCount} neutralCount={neutralCount} badCount={badCount} />
+    </div>
+
+  )
+}
+
+const Statistics = ({ goodCount, neutralCount, badCount }) => {
+  return (
+    <div>
       <Display value={"all"} counter={goodCount + neutralCount + badCount} />
       <Display value={"average"} counter={(goodCount - badCount) / (goodCount + neutralCount + badCount)} />
       <Display value={"positive"} counter={goodCount / (goodCount + neutralCount + badCount) + " %"} />
-     </div>
-     
+    </div>
   )
 }
 
 const Display = ({ value, counter }) => (
-    <div>
-      <p> {value} {counter} </p>
-    </div>
+  <div>
+    <p> {value} {counter} </p>
+  </div>
 )
-
 
 const Button = ({ handleClick, text }) => {
   return (
