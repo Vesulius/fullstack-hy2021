7961182f931a3df72d5f893cac77155ec606d9a5
Vesa <vesainwonderland@gmail.com>
2021-07-28 15:00:55 +0300

    4.5-4.7 apufunktioita ja yksikkotesteja

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -3,11 +3,30 @@ const dummy = () => {
 }
 
 const totalLikes = blogs => {
-  console.log(typeof blogs)
   return blogs.reduce((total, blog) => total + blog.likes, 0)
+}
+
+const favoriteBlog = blogs => {
+  return blogs.reduce((favorite, blog) => {
+    return (!favorite || blog.likes > favorite.likes) ? blog : favorite
+  })
+}
+
+const mostBlogs = blogs => {
+  return blogs
+    .map(blog => {return { author: blog.author, blogs: 1 }})
+    .reduce((list, blog) => {
+      const index = list.findIndex(b => b.author === blog.author)
+      if (index === -1) return list.concat(blog)
+      list[index].blogs++
+      return list
+    }, [])
+    .reduce((most, author) => {return (!most || author.blogs > most.blogs) ? author : most})
 }
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
+  mostBlogs
 }
