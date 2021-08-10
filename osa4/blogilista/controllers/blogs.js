57c6c36393952b359bbb4f1bf5cc3d5d23903cef
Vesa <vesainwonderland@gmail.com>
2021-08-10 17:21:58 +0300

    4.19 blogilistan laajennus

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', (request, response, next) => {
   Blog
@@ -32,7 +33,14 @@ blogsRouter.get('/', (request, response, next) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const user = await User.findOne({})
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
@@ -74,5 +82,13 @@ blogsRouter.put('/:id', async (request, response, next) => {
     next(error)
   }
 })
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 module.exports = blogsRouter
