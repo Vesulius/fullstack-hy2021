11b5c5c302df73e837e077c2ef581597f2bec9a9
Vesa <vesainwonderland@gmail.com>
2021-08-11 13:39:00 +0300

    4.20 blogilistan laajennus

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -33,10 +33,9 @@ blogsRouter.get('/', (request, response, next) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
-  if (!token || !decodedToken.id) {
+  if (!request.token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
 
@@ -82,13 +81,5 @@ blogsRouter.put('/:id', async (request, response, next) => {
     next(error)
   }
 })
-
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
 
 module.exports = blogsRouter
