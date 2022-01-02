f812e6142d8db21d75dc5ad8dafceae196920a25
vesa <vesainwonderland@gmail.com>
2022-01-02 12:36:36 +0200

    6.2 unicafe revisited

--- osa6/unicafe-redux/src/index.js
+++ osa6/unicafe-redux/src/index.js
@@ -6,38 +6,20 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
+  const storeDispatch = ( use ) => {
     store.dispatch({
-      type: 'GOOD'
+      type: use
     })
   }
-
-  const ok = () => {
-    store.dispatch({
-      type: 'OK'
-    })
-  }
-
-  const bad = () => {
-    store.dispatch({
-      type: 'BAD'
-    })
-  }
-
-  const reset = () => {
-    store.dispatch({
-      type: 'ZERO'
-    })
-  }
-
+  
   return (
     <div>
-      <button onClick={good}>good</button>
-      <button onClick={ok}>ok</button>
-      <button onClick={bad}>bad</button>
-      <button onClick={reset}>reset stats</button>
+      <button onClick={() => storeDispatch('GOOD')}>good</button> 
+      <button onClick={() => storeDispatch('OK')}>neutral</button> 
+      <button onClick={() => storeDispatch('BAD')}>bad</button>
+      <button onClick={() => storeDispatch('ZERO')}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok {store.getState().ok}</div>
+      <div>neutral {store.getState().ok}</div>
       <div>bad {store.getState().bad}</div>
     </div>
   )
