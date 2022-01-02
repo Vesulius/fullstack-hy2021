d4d0e9a3fb73dee11590be82e8610d4e4779884b
vesa <vesainwonderland@gmail.com>
2022-01-02 18:51:14 +0200

    6.5-6.6 anekdootit

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote, addAnecdote } from './reducers/anecdoteReducer'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => state.sort((a1, a2) => a1.votes-a2.votes).reverse())
   const dispatch = useDispatch()
 
   const vote = (id) => {
