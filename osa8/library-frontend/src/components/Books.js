8ed829348ec4b61d261432f9d0fe8d357a1df0fe
vesa <vesainwonderland@gmail.com>
2022-03-18 15:10:40 +0200

    8.19

--- osa8/library-frontend/src/components/Books.js
+++ osa8/library-frontend/src/components/Books.js
@@ -1,13 +1,46 @@
 import { useQuery } from "@apollo/client"
+import { useState } from "react"
 import { ALL_BOOKS } from "../queries"
 
 const Books = (props) => {
+  const [genre, setGenre] = useState("")
   const result = useQuery(ALL_BOOKS)
   if (!props.show || !result.data) {
     return null
   }
-  const books = result.data.allBooks
-  
+
+  const allBooks = result.data.allBooks
+
+  const books = () => {
+    if (genre === "") {
+      return allBooks
+    }
+    const filtered = allBooks.filter(b => b.genres.some(g => g === genre))
+    return filtered
+  }
+
+  const allGenres = () => {
+    const genres = allBooks
+      .map((b) => b.genres)
+      .flat()
+    return [...new Set(genres)]
+  }
+
+  const selected = (buttonGenre) => {
+    if (genre === buttonGenre) {
+      return { color: "darkblue" }
+    }
+    return { color: "dodgerblue" }
+  }
+
+  const buttonAction = (buttonGenre) => {
+    if (genre === buttonGenre) {
+      setGenre("")
+    } else {
+      setGenre(buttonGenre)
+    }
+  }
+
   return (
     <div>
       <h2>books</h2>
@@ -15,11 +48,11 @@ const Books = (props) => {
       <table>
         <tbody>
           <tr>
-            <th></th>
+            <th>title</th>
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {books().map((a) => (
             <tr key={a.id}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -28,7 +61,13 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
-      
+      <div>
+        {allGenres().map((g) => (
+          <button key={g} style={selected(g)} onClick={() => buttonAction(g)}>
+            {g}
+          </button>
+        ))}
+      </div>
     </div>
   )
 }
